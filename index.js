0b949e85275afab966e28e34d8e131db1a3ebef7
vaibhav7000 <vchawla7000@gmail.com>
2024-04-17 14:15:56 +0530

    added home route in index

--- index.js
+++ index.js
@@ -14,6 +14,10 @@ application.listen(process.env.PORT,() => {
   console.log('sever is started at 3000 port');
 });
 
+application.get('/' , (req,res) =>{
+  res.send('Your are present on home page');
+})
+
 const personRoutes = require('./routes/Person.routes');
 const menuRouter = require('./routes/menu.routes');
 
@@ -30,3 +34,5 @@ application.use('/menu' , menuRouter);
 // the dealing with the server takes times => wait until it is not completed
 
 // body parser keeps the data from the client into the req.body as a object and make it simple for server to process the data that comes from the frontend and it called middleware and the format in which we want to keep the data is told to the bodyparser in our case we want the data in the form of json
+
+// project0 -> DB -> cluster0 -> Menu and Person
